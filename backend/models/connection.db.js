5fba6da91e015545aa19d68e113f84f4b82dbe57
= <jose.s.machava@gmail.com>
2019-11-28 22:09:25 +0200

    remove files

--- backend/models/connection.db.js
+++ backend/models/connection.db.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect("mongodb+srv://jose:849394995@cluster0-ltdya.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true ,  useUnifiedTopology: true }
-);
+
+console.log(uri)
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true ,  useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
